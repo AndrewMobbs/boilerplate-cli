ba33ad4485bd9c12446f9b5635325324ac88785e
Andrew Mobbs <andrew.mobbs@uk.thalesgroup.com>
2021-07-08 09:50:55 +0100

    add example of parameter with constrained valid options

--- app/example.go
+++ app/example.go
@@ -1,5 +1,11 @@
 package app
 
+const (
+	EnumParamFoo = "foo"
+	EnumParamBar = "bar"
+	EnumParamBaz = "baz"
+)
+
 // The logic for the "example" command, separated from CLI plumbing
 func (a *App) Example() error {
 	a.Logger.Trace("Example()")
@@ -7,8 +13,8 @@ func (a *App) Example() error {
 	a.Logger.Info("Ran example command")
 	return nil
 }
-func (a *App) SubCommand(fixedParam string, flagParam int) error {
-	a.Logger.Tracef("SubCommand(%s,%d)", fixedParam, flagParam)
+func (a *App) SubCommand(fixedParam string, flagParam int, enumParam string) error {
+	a.Logger.Tracef("SubCommand(%s,%d)", fixedParam, flagParam, enumParam)
 	a.Logger.Warn("Ran SubCommand")
 	return nil
 }
--- cli/subcommand.go
+++ cli/subcommand.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"os"
+
 	"github.com/AndrewMobbs/boilerplate-golang-cli/app"
 	"github.com/spf13/cobra"
 )
@@ -9,10 +11,38 @@ type ExampleSubCommand struct {
 	App        *app.App
 	flagParam  int
 	fixedParam string
+	enumParam  string
+}
+
+type EnumParam struct {
+	action string
+}
+
+func (a *EnumParam) String() string {
+	return a.action
+}
+
+func (a *EnumParam) Type() string {
+	return "string"
+}
+
+func (a *EnumParam) Set(s string) error {
+	switch s {
+	case app.EnumParamFoo,
+		app.EnumParamBar,
+		app.EnumParamBaz:
+		a.action = s
+		return nil
+	case "": // Default value
+		a.action = app.EnumParamFoo
+		return nil
+	}
+	return os.ErrInvalid
 }
 
 // An example subcommand with positional and flag arguments
 func (c *ExampleSubCommand) Command() *cobra.Command {
+	var enumParam EnumParam
 	subCmd := &cobra.Command{
 		Use:   "subcommand <string>",
 		Short: "A brief description of your command",
@@ -21,12 +51,14 @@ and usage of using your command. `,
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			c.fixedParam = args[0]
-			c.App.SubCommand(c.fixedParam, c.flagParam)
+			c.enumParam = enumParam.action
+			c.App.SubCommand(c.fixedParam, c.flagParam, c.enumParam)
 		},
 	}
 
 	// Add the flags here
-	subCmd.Flags().IntVarP(&c.flagParam, "flagparam", "f", 5, "Example parameter supplied by flag")
+	subCmd.Flags().IntVarP(&c.flagParam, "flag-param", "f", 5, "Example parameter supplied by flag")
+	subCmd.Flags().VarP(&enumParam, "enum-param", "e", "Parameter with constrained valid values; foo|bar|baz (default foo)")
 
 	// Add subcommands here.
 
